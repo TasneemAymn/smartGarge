d1c5d12382a31b5faee8048016735cdb271fd15d
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2024-08-20 03:50:04 +0300

    garageBlynk.ino

--- garageBlynk.ino
+++ garageBlynk.ino
@@ -145,3 +145,203 @@ void loop() {
   Serial.println(entryState);
   Serial.print("Exit Sensor State: ");
   Serial.println(exitState);
+ if (energySavingMode && lightState == 0) {
+// Energy saving mode: light on if car entry/exit detected(with reversed logic)
+    if (entryState == LOW || exitState == LOW) {
+      for (int i = 0; i < 2; i++) {  
+        digitalWrite(ledPins[i], HIGH); 
+      }
+    }else {
+      for (int i = 0; i < 2; i++) {  
+        digitalWrite(ledPins[i], LOW); 
+      }
+    }
+  } else {
+    // Normal mode: light based on LDR value
+    if (ldrValue > 600) {
+      if (!lightOn) {
+        Blynk.logEvent("light_on", "Light on! in normal mode");
+        lightOn = true;
+      }
+      lightOff = false;
+      for (int i = 0; i < 2; i++) {  
+        digitalWrite(ledPins[i], HIGH); 
+      }
+    } else {
+      if (!lightOff) {
+        Blynk.logEvent("light_off", "Light off! in normal mode");
+        lightOff = true;
+      }
+      lightOn = false;
+      for (int i = 0; i < 2; i++) {  
+        digitalWrite(ledPins[i], LOW); 
+      }
+    }
+  }
+
+  // Gate system  
+  bool carEntering = false;
+  bool carExiting = false;
+  // Entering car
+  if (entryState == LOW && doorState == 0 && carCount < 5 && !carEntering && !carInProgress) {
+     if (currentMillis - lastEntryCheck >= debounceDelay) {  
+      lastEntryCheck = currentMillis;
+
+      Serial.println("Car entering...");
+      carInProgress = true;  // Car started entering
+
+      for (int m = 0; m <= 90; m++) {
+        Garagedoor.write(m);
+        delay(10);
+      }
+      delay(1000);
+
+      // Wait for the car to pass the entry sensor
+      while (digitalRead(irSensorEntry) == LOW) {
+        delay(500);
+      }
+
+      // Check if the car has passed the exit sensor
+      if (digitalRead(irSensorExit) == LOW) {
+        carCount++;
+        Serial.print("Car Count increased: ");
+        Serial.println(carCount);
+        carEntering = true;
+      }
+
+      for (int m = 90; m >= 0; m--) {
+        Garagedoor.write(m);
+        delay(10);
+      }
+
+      carInProgress = false;  // Car has fully entered
+
+      // temporary LCD for entering car 
+      centerMessage("Welcome!");
+      delay(1000);
+    }
+  } else if (entryState == HIGH) {
+    carEntering = false;
+  }
+
+  // Entering the car and the garage is full
+  if (entryState == LOW && doorState == 0 && carCount >= 5) {
+    // temporary LCD for ettempting car to enter but garage is already full
+    centerMessage("Garage full!");
+    delay(1000); 
+  } 
+
+  // Exiting the car
+  else if (exitState == LOW && doorState == 0 && carCount > 0 && !carExiting && !carInProgress) {
+    if (currentMillis - lastExitCheck >= debounceDelay) { 
+      lastExitCheck = currentMillis;
+
+      Serial.println("Car exiting...");
+      carInProgress = true;  // Car started exiting
+
+      for (int n = 0; n <= 90; n++) {
+        Garagedoor.write(n);
+        delay(10);
+      }
+      delay(1000);
+
+      // Wait for the car to pass the exit sensor
+      while (digitalRead(irSensorExit) == LOW) {
+        delay(500);
+      }
+
+      // Check if the car has passed the enter sensor
+      if (digitalRead(irSensorEntry) == LOW) {
+        carCount--;
+        Serial.print("Car Count decreased: ");
+        Serial.println(carCount);
+        carExiting = true;
+      }
+
+      for (int n = 90; n >= 0; n--) {
+        Garagedoor.write(n);
+        delay(10);
+      }
+
+      carInProgress = false;  // Car has fully exited
+
+      //temporary LCD for exiting car
+      centerMessage("Goodbye!");
+      delay(1000);
+    }
+  }else if (exitState == HIGH) {
+    carExiting = false;
+  }
+
+  // Collision detection
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.035 / 2;
+
+  // Print distance that b/w the car and the sidewalk to Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance < 10) {
+    tone(buzzerPin, 800);
+delay(1000);
+    noTone(buzzerPin);    
+    delay(1000);
+    // temporary LCD for collision alert
+    centerMessage("Collision alert!");
+    delay(1000);
+    if (!collision) {
+      Blynk.logEvent("collision_alert", "Car is close to the sidewalk! Alarm ON.");
+      collision = true;
+    }
+  } else {
+    collision = false;
+    noTone(buzzerPin);
+  }
+
+  // Fire Detection
+  int flameValue = analogRead(flamePin);  
+
+  //Print Flame Value to Serial Monitor
+  Serial.print("Flame Value: ");
+  Serial.println(flameValue);
+
+  if (flameValue > 200) {  
+    tone(buzzerPin, 800);
+    delay(1000);
+    noTone(buzzerPin);    
+    delay(1000);
+    // temporary LCD for fire alert
+    centerMessage("Fire detected!");
+    delay(1000);
+    if (!fire) {
+      Blynk.logEvent("fire_detected", "Fire detected! Alarm ON.");
+      fire = true;
+    }
+  } else {
+    fire = false;
+    noTone(buzzerPin);
+  }
+
+  // Empty places LCD
+  if (digitalRead(irSensorExit) == HIGH && digitalRead(irSensorEntry) == HIGH && flameValue <= 200 && distance >= 10) {
+      lcd.setCursor(0, 1);
+      lcd.print("Empty places : ");
+      lcd.print(String(5 - carCount));
+    }
+
+  // Permanent LCD message 
+  lcd.setCursor(4, 0);
+  lcd.print("ER Garage");
+
+  // Serial Communication Error Handling
+    if (!espSerial.available()) {
+      Serial.println("Error: No data from ESP8266");
+    } else {
+      Serial.println("Data received from ESP8266");
+    }
+  }
